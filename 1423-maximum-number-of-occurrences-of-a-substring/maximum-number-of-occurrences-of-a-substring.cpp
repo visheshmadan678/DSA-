2d958ee898b88a03c1457d1e9957e47288a4ec2c
vishesh madan <119437780+visheshmadan678@users.noreply.github.com>
2025-07-23 16:50:19 +0530

    Time: 784 ms (11.42%) | Memory: 102.7 MB (21.75%) - LeetSync

---
+++ 1423-maximum-number-of-occurrences-of-a-substring/maximum-number-of-occurrences-of-a-substring.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    int maxFreq(string s, int maxLetters, int minSize, int maxSize) {
+        // i want the substring of length minsize all the substrings of length
+        // min size and return the maximum of them
+        int n = s.length();
+        unordered_map<string, int> mp;
+        int ans = INT_MIN;
+        for (int i = 0; i <= n - minSize; i++) {
+            string temp = s.substr(i, minSize); // take the length of the string as min size
+            cout<<temp<<endl;
+            unordered_set<char> st(temp.begin(), temp.end());
+            if (st.size() <= maxLetters)
+                mp[temp]++;
+        }
+
+        for(auto it:mp){
+            ans = max(it.second,ans);
+        }
+        return ans==INT_MIN?0:ans;
+    }
+};
