eaebef3e506c75d5cd977ba10b16ce39b5b576a7
vishesh madan <119437780+visheshmadan678@users.noreply.github.com>
2025-08-04 09:52:32 +0530

    Time: 855 ms (17.25%) | Memory: 257 MB (13.27%) - LeetSync

---
+++ 2174-next-greater-numerically-balanced-number/next-greater-numerically-balanced-number.cpp
@@ -0,0 +1,27 @@
+class Solution {
+public:
+    bool valid(int num){
+        //just check that number 
+        string str = to_string(num);
+        //i think the digits can be only 1 to 9 so i can iterate through an array of digits always 
+        vector<int>digits(10,0);
+        for(char &it:str){
+            digits[it-'0']++; // just increase the amount
+        }
+        for(int i=0;i<10;i++){
+            if(digits[i]!=0 && digits[i]!=i)return false;
+        }
+        return true;
+    }
+    int nextBeautifulNumber(int n) {
+        int i = n+1;
+        while(true){
+            if(valid(i)){
+                return i;
+                //just return true from here on the first valid part 
+            }
+            i++; // increase the i;
+        }
+        return 0;
+    }
+};
