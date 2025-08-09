2f1b46f8d3abdc37e40edafd9e13f8d1e1f3c333
vishesh madan <119437780+visheshmadan678@users.noreply.github.com>
2025-08-09 11:01:17 +0530

    Time: 0 ms (100.00%) | Memory: 7.9 MB (17.71%) - LeetSync

---
+++ 231-power-of-two/power-of-two.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    bool isPowerOfTwo(int n) {
+        if(n==1)return true;
+        if(n<0)return false; // return false straight away not a valid case
+        int i=0;
+        long long exp = pow(2,i);
+        while(exp<n){
+            i++;
+            exp = pow(2,i);
+        } 
+        return exp == n;
+    }
+};
