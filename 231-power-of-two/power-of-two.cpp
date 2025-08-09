8628af66560d73e77317ccad63e989be6268ff7d
vishesh madan <119437780+visheshmadan678@users.noreply.github.com>
2025-08-09 11:01:29 +0530

    Time: 0 ms (100.00%) | Memory: 7.9 MB (17.71%) - LeetSync

--- 231-power-of-two/power-of-two.cpp
+++ 231-power-of-two/power-of-two.cpp
@@ -1,6 +1,7 @@
 class Solution {
 public:
     bool isPowerOfTwo(int n) {
+        //code by vishu badmos 
         if(n==1)return true;
         if(n<0)return false; // return false straight away not a valid case
         int i=0;
