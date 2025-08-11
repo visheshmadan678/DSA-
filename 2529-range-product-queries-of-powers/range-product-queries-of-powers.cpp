c6d1dabd7cbe833d9d8cc92ab98ad451135f7b71
vishesh madan <119437780+visheshmadan678@users.noreply.github.com>
2025-08-11 12:57:02 +0530

    Update range-product-queries-of-powers.cpp

--- 2529-range-product-queries-of-powers/range-product-queries-of-powers.cpp
+++ 2529-range-product-queries-of-powers/range-product-queries-of-powers.cpp
@@ -1,6 +1,8 @@
 class Solution {
 public:
     static const int mod = 1e9 + 7; // take the mod
+    //code by vishu badmos
+    //badmashi nahi mitar 
     vector<int> productQueries(int n, vector<vector<int>>& queries) {
         vector<long long> nums;
         for (int i = 0; i <= 31; i++) {
@@ -8,17 +10,11 @@ public:
                 nums.push_back(1 << i); // push that power  into the nums
             }
         }
-        vector<long long> products(nums.size());
-        products[0] = nums[0] % mod;
-
         vector<int>ans;
         for(auto it:queries){
-            int start = it[0]; // take the start point
-            int end = it[1];  // take the end point
+            int start = it[0], end = it[1]; // take the start point  and take the end point
             long long product = 1;
-            for(int  i=start;i<=end;i++){
-                product = (product * nums[i]) % mod;
-            }
+            for(int  i=start;i<=end;i++)product = (product * nums[i]) % mod;
             ans.push_back(product % mod);
         }
         return ans;
