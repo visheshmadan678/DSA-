ed987b6172d54ec8fb9ae0941f1c98e118a4ddc9
vishesh madan <119437780+visheshmadan678@users.noreply.github.com>
2025-08-27 14:14:48 +0530

    Time: 542 ms (5.21%) | Memory: 209.1 MB (5.21%) - LeetSync

--- 3395-minimum-length-of-anagram-concatenation/minimum-length-of-anagram-concatenation.cpp
+++ 3395-minimum-length-of-anagram-concatenation/minimum-length-of-anagram-concatenation.cpp
@@ -1,43 +1,40 @@
 class Solution {
 public:
-    bool check(int length,vector<int>&nums,string &s){
-        //now i have to check if i can divide the s into s / len substrings and 
-        vector<int>vec(26,0);
-        for(int i=0;i<length;i++)vec[s[i]-'a']++;  // add the frequency to the vector
-        int ratio = -1;
-        for(int i=0;i<26;i++){
-            if(nums[i]!=0 && vec[i]!=0 && ratio==-1){
-                ratio = nums[i] / vec[i]; // store the ratio
+    bool check(int length, string &s) {
+        int n = s.size();
+        if (n % length != 0) return false;
+
+        // map of frequency for the first window
+        unordered_map<char,int> ref;
+        for (int i = 0; i < length; i++) {
+            ref[s[i]]++;
+        }
+
+        // check every next window
+        for (int i = length; i < n; i += length) {
+            unordered_map<char,int> curr;
+            for (int j = i; j < i + length; j++) {
+                curr[s[j]]++;
             }
-            else if(nums[i]!=0 && vec[i]!=0){
-                int r = nums[i]/vec[i];
-                if(r!=ratio)return false;
-                else continue;
-            }
-            else if((nums[i]==0 && vec[i]!=0) || (nums[i]!=0 && vec[i]==0) )return false;
+            if (curr != ref) return false; // window differs
         }
         return true;
     }
+
     int minAnagramLength(string s) {
         int len = s.length();
-        if(s=="abbbaa")return 6;
-        if(s=="ababaabb")return 4;
-        vector<int>factors;
-        vector<int>nums(26,0);
-        for(auto it:s)nums[it-'a']++; // store the count of every alphabet as a global map
+        vector<int> factors;
 
-        //finding all the factors in root n time 
-        for(int f = 1;  f * f <= len; f++){
-            if(len%f!=0)continue;
-            int f1 = f , f2 = len / f1; // these both will be the factors 
-            factors.push_back(f1);
-            if(f1!=f2)factors.push_back(f2); // then i can also push f2 also  
+        // find all factors in O(√n)
+        for (int f = 1; f * f <= len; f++) {
+            if (len % f != 0) continue;
+            factors.push_back(f);
+            if (f != len / f) factors.push_back(len / f);
         }
 
-        // now i have all the factors inside the factors vector;
         int ans = INT_MAX;
-        for(auto it:factors){
-            if(check(it,nums,s))ans =  min(ans,it); // if it's a valid answer
+        for (auto it : factors) {
+            if (check(it, s)) ans = min(ans, it);
         }
 
         return ans;
