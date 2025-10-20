bb34b2330c820108113b059caf2d84b67418fef9
vishesh madan <119437780+visheshmadan678@users.noreply.github.com>
2025-10-20 10:26:57 +0530

    Time: 0 ms (100.00%) | Memory: 17.7 MB (42.92%) - LeetSync

---
+++ 2137-final-value-of-variable-after-performing-operations/final-value-of-variable-after-performing-operations.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    int finalValueAfterOperations(vector<string>& operations) {
+        int x = 0;
+        for(auto &it:operations){
+            if(it == "--X" || it == "X--"){
+                x -= 1;
+            }
+            else{
+                x += 1;
+            }
+        }
+        return x;
+    }
+};
