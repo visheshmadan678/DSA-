487e963bbb6dc43ee6059e4c002c8166aa78c64e
vishesh madan <119437780+visheshmadan678@users.noreply.github.com>
2025-12-26 10:16:26 +0530

    Time: 7 ms (67.76%) | Memory: 29.8 MB (5.79%) - LeetSync

---
+++ 2576-minimum-penalty-for-a-shop/minimum-penalty-for-a-shop.cpp
@@ -0,0 +1,54 @@
+class Solution {
+public:
+    int bestClosingTime(string customers) {
+        int size = customers.size(); // represents the size of our array
+        vector<long long>prefix(size+1); //will make this array on the basis of 
+        vector<long long>suffix(size);
+
+        if(customers[0] == 'N'){
+            prefix[0] = 1;
+        }
+        else prefix[0] = 0; 
+        
+        for(int i=1;i<size+1;i++){
+            if(customers[i] == 'N'){
+                prefix[i] = 1 + prefix[i-1];
+            }
+            else prefix[i] = prefix[i-1]; // remain as it is
+        }
+
+        if(customers[size-1] == 'Y'){
+            suffix[size-1] = 1;
+        }
+        else suffix[size-1] = 0;
+
+        for(int i=size-2;i>=0;i--){
+            if(customers[i] == 'Y'){
+                suffix[i] = 1 + suffix[i+1];
+            }
+            else suffix[i] = suffix[i+1];
+        }
+
+
+        long long ans = 0; // i will store the index 
+        long long penalty = INT_MAX; //it will hold the value of penalty
+
+        for(int i=0;i<size+1;i++){
+            long long curr = 0;
+            if(i-1>=0){
+                curr += prefix[i-1]; // number of times when the shop was open and no one came
+            }
+
+            if(i < size){
+                curr += suffix[i]; // number of times when the shop was closed and everybody came 
+            }
+
+            if(curr < penalty){
+                penalty = curr;
+                ans = i; // representing the ith hour 
+            }
+        }
+        
+        return ans;
+    }
+};
