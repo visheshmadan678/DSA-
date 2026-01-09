2b7074746f5e51d79041ddeed6e7e8bce8729773
vishesh madan <119437780+visheshmadan678@users.noreply.github.com>
2026-01-09 12:06:37 +0530

    Time: 0 ms (100.00%) | Memory: 16.2 MB (52.91%) - LeetSync

---
+++ 896-smallest-subtree-with-all-the-deepest-nodes/smallest-subtree-with-all-the-deepest-nodes.cpp
@@ -0,0 +1,45 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    pair<TreeNode*,long long> dfs(TreeNode *root, long long depth){
+        if(!root)return {nullptr,0};
+        pair<TreeNode*,long long>left = dfs(root->left,1 + depth); // the left answer
+        pair<TreeNode*,long long>right = dfs(root->right,1 + depth); // the right answer
+
+        if(left.first && right.first){
+            if(left.second == right.second){
+                return {root,left.second}; // return this root with the answer
+            }
+            else if(left.second > right.second){
+                return {left.first,left.second};
+            }
+            else{
+                return {right.first,right.second};
+            }
+        }
+
+        if(left.first){
+            //if only left is present
+            return {left.first, left.second};
+        }
+
+        if(right.first){
+            return {right.first,right.second};
+        }
+
+        return {root,depth}; // if both left and right are not present return root with current depth
+    }
+    TreeNode* subtreeWithAllDeepest(TreeNode* root) {
+        return dfs(root,0).first;
+    }
+};
