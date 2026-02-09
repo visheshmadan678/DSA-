d808f8a2435eed58475d2f215cff27c3e9e91bc4
vishesh madan <119437780+visheshmadan678@users.noreply.github.com>
2026-02-09 15:22:22 +0530

    Time: 16 ms (5.36%) | Memory: 103.2 MB (5.16%) - LeetSync

---
+++ 4003-longest-fibonacci-subarray/longest-fibonacci-subarray.cpp
@@ -0,0 +1,27 @@
+class Solution {
+public:
+    int dp[100001];
+
+    int recursion(int idx, int next_idx, vector<int>& nums) {
+        if (idx == nums.size())
+            return 0;
+        if (dp[idx] != -1)
+            return dp[idx];
+
+        int ans = 0;
+        if (next_idx+1 < nums.size() &&
+            (nums[idx] + nums[next_idx]) == nums[next_idx + 1]) {
+            ans = 1 + recursion(next_idx, next_idx + 1, nums);
+        }
+        return dp[idx] = ans;
+    }
+    int longestSubarray(vector<int>& nums) {
+        memset(dp, -1, sizeof(dp));
+        int ans = INT_MIN;
+        for (int i = 0; i < nums.size() - 1; i++) {
+            ans = max(ans, 2 + recursion(i, i + 1, nums));
+        }
+
+        return ans;
+    }
+};
