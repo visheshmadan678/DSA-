f1b1a3e4f85dc8b29e212ad5b668f06708b8bbf2
vishesh madan <119437780+visheshmadan678@users.noreply.github.com>
2026-02-10 14:46:39 +0530

    Time: 55 ms (99.66%) | Memory: 292.5 MB (53.36%) - LeetSync

---
+++ 4059-design-exam-scores-tracker/design-exam-scores-tracker.cpp
@@ -0,0 +1,49 @@
+class ExamTracker {
+private:
+    vector<pair<int, long long>> arr;
+    long long fn(vector<pair<int, long long>>& arr, int time) {
+        long long total= 0;
+
+        int left = 0, right = arr.size() - 1;
+        int idx = -1;
+
+        while (left <= right) {
+            int mid = (left + right) / 2;
+            if (arr[mid].first <= time) {
+                idx = mid;
+                left = mid + 1;
+            } else {
+                right = mid - 1;
+            }
+        }
+        if (idx != -1)
+            total = arr[idx].second;
+
+        return total;
+    }
+
+    public:
+    ExamTracker() {}
+
+    void record(int time, int score) {
+        long long ans = score;
+        if (!arr.empty()) {
+            ans += arr.back().second;
+        }
+        arr.push_back({time, ans});
+    }
+
+    long long totalScore(int start, int end) {
+        long long left = fn(arr, start - 1);
+        long long right = fn(arr, end);
+
+        return right - left;
+    }
+};
+
+/**
+ * Your ExamTracker object will be instantiated and called as such:
+ * ExamTracker* obj = new ExamTracker();
+ * obj->record(time,score);
+ * long long param_2 = obj->totalScore(startTime,endTime);
+ */
