01d656ffbf7694543d0232ac721082775287b3d5
vishesh madan <119437780+visheshmadan678@users.noreply.github.com>
2026-03-16 20:02:59 +0530

    Time: 413 ms (20.00%) | Memory: 338.2 MB (6.67%) - LeetSync

---
+++ 4253-minimum-cost-to-equalize-arrays-using-swaps/minimum-cost-to-equalize-arrays-using-swaps.cpp
@@ -0,0 +1,49 @@
+class Solution {
+public:
+    int minCost(vector<int>& nums1, vector<int>& nums2) {
+
+        unordered_map<int,int> mp1;
+        unordered_map<int,int> mp2;
+        unordered_map<int,pair<int,int>> mp3;
+
+        for(auto &it:nums1){
+            mp1[it]++;
+        }
+
+        for(auto &it:nums2){
+            mp2[it]++;
+        }
+
+        // fill mp3 using mp1
+        for(auto &it:mp1){
+            int ele = it.first;
+            int freq1 = it.second;
+            int freq2 = mp2[ele];
+            mp3[ele] = {freq1,freq2};
+        }
+
+        // add remaining elements from mp2
+        for(auto &it:mp2){
+            int ele = it.first;
+            int freq2 = it.second;
+            int freq1 = mp1[ele];
+
+            if(mp3.find(ele) == mp3.end()){
+                mp3[ele] = {freq1,freq2};
+            }
+        }
+
+        int ans = 0;
+
+        for(auto &it:mp3){
+            int a = it.second.first;
+            int b = it.second.second;
+
+            if((a + b) % 2 != 0) return -1;
+
+            ans += abs(a - b) / 2;
+        }
+
+        return ans / 2;
+    }
+};
