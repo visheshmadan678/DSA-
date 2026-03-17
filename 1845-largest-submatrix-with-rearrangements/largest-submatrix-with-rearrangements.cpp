6b8b211a0586f7583ae2fd81a9e087b50d517dc7
vishesh madan <119437780+visheshmadan678@users.noreply.github.com>
2026-03-17 13:59:04 +0530

    Time: 79 ms (13.30%) | Memory: 96.8 MB (20.74%) - LeetSync

---
+++ 1845-largest-submatrix-with-rearrangements/largest-submatrix-with-rearrangements.cpp
@@ -0,0 +1,45 @@
+class Solution {
+public:
+    int largestSubmatrix(vector<vector<int>>&nums) {
+        int ans = 0;
+        int m = nums.size();
+        int n = nums[0].size();
+
+        // take the prefix sum column wise for every column
+        vector<vector<int>> prefix(nums.size(), vector<int>(nums[0].size(), 0));
+
+        for (int i = 0; i < n; i++) {
+            if (nums[m - 1][i] == 1) {
+                prefix[m - 1][i] = 1;
+            } else {
+                prefix[m - 1][i] = 0;
+            }
+        }
+
+        // i is for column and j is for rows
+        for (int i = 0; i < n; i++) {
+            for (int j = m - 2; j >= 0; j--) {
+                if (nums[j][i] == 1) {
+                    prefix[j][i] = 1 + prefix[j + 1][i];
+                } else {
+                    prefix[j][i] = 0;
+                }
+            }
+        }
+
+        for (auto& it : prefix) {
+            sort(it.rbegin(), it.rend());
+        }
+
+        for (int i = 0; i < m; i++) {
+            int length = 1;
+            for (int j = 0; j < n; j++) {
+                int ele = prefix[i][j];
+                ans = max(ans, ele * length);
+                length++;
+            }
+        }
+
+        return ans;
+    }
+};
