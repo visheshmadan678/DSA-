64561a276a8acc3794cb29ef014190f31b353eb0
vishesh madan <119437780+visheshmadan678@users.noreply.github.com>
2026-04-21 16:15:46 +0530

    Time: 0 ms (100.00%) | Memory: 13 MB (6.92%) - LeetSync

--- 2199-two-furthest-houses-with-different-colors/two-furthest-houses-with-different-colors.cpp
+++ 2199-two-furthest-houses-with-different-colors/two-furthest-houses-with-different-colors.cpp
@@ -1,15 +1,35 @@
 class Solution {
 public:
     int maxDistance(vector<int>& colors) {
+        //first number from the back which is not equal to my current number
+        //so store the last occurence for every different type of color and if 
+        // last occurence is at the last then return n - 2 as the answer else return n - 1 as the answer
+        unordered_map<int,vector<int>>mp;
+
+        for(int i=0;i<colors.size();i++){
+            int color = colors[i];
+            mp[color].push_back(i);
+        }
+
         int ans = INT_MIN;
 
-        for(int i=0;i < colors.size();i++){
-            int curr = colors[i];
-            for(int j = i+1; j < colors.size();j++){
-                int curr2 = colors[j];
-                if(curr != curr2){
-                    ans = max(ans,j - i);
-                }
+        for(auto &it:mp){
+            int current_color = it.first;
+            vector<int>indices = it.second; int size = indices.size(); int last = colors.size() - 1;
+            int last_index = indices[size-1]; int first = indices[0];
+
+            if(last_index != last){
+                ans = max(ans,last - first); // this can be my answer directly
+            } 
+            else{
+                //if the last index is equal to last it means that element is present in the last also 
+                for(int i=size-1;i>=1;i--){
+                    int curr = indices[i-1]; int next = indices[i];
+                    if(abs(curr - next) != 1){
+                        //it means i have my answer here itself
+                        ans = max(ans,next-1);
+                    } 
+                } 
             }
         }
 
