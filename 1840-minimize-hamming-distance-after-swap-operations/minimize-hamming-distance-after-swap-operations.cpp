e687a3d4f6ff48f7a519f0cd73e825ce7fe66ff9
vishesh madan <119437780+visheshmadan678@users.noreply.github.com>
2026-04-23 21:38:47 +0530

    Time: 282 ms (6.12%) | Memory: 197.9 MB (5.42%) - LeetSync

--- 1840-minimize-hamming-distance-after-swap-operations/minimize-hamming-distance-after-swap-operations.cpp
+++ 1840-minimize-hamming-distance-after-swap-operations/minimize-hamming-distance-after-swap-operations.cpp
@@ -1,57 +1,61 @@
 class Solution {
 public:
-    void dfs(int node, int parent_node,
-             unordered_map<int, unordered_map<int,int>>& mp,
-             unordered_map<int, vector<int>>& graph,
-             vector<int>& visited,
-             vector<int>& parent,
-             vector<int>& source) {
+    void dfs(int node,int parent_node,unordered_map<int,unordered_map<int,int>>&mp,unordered_map<int,vector<int>>&graph,vector<int>&visited,vector<int>&parent,vector<int>&source){
 
-        visited[node] = 1;
-        parent[node] = parent_node;
+        visited[node] = 1; // mark the node as visited 
         mp[parent_node][source[node]]++;
+        parent[node] = parent_node;
 
-        for (auto &it : graph[node]) {
-            if (!visited[it]) {
-                dfs(it, parent_node, mp, graph, visited, parent, source);
+        for(auto &it: graph[node]){
+            if(!visited[it]){
+                dfs(it,parent_node,mp,graph,visited,parent,source);
             }
         }
+
+        return;
     }
-
     int minimumHammingDistance(vector<int>& source, vector<int>& target, vector<vector<int>>& allowedSwaps) {
+        unordered_map<int,unordered_map<int,int>>mp;
+        vector<int>parent(source.size(),-1); // initially subka baap minus one hai lets suppose
 
-        int n = source.size();
+        unordered_map<int,vector<int>>graph;
 
-        unordered_map<int, unordered_map<int,int>> mp;
-        unordered_map<int, vector<int>> graph;
-
-        for (auto &it : allowedSwaps) {
-            int u = it[0];
-            int v = it[1];
-            graph[u].push_back(v);
-            graph[v].push_back(u);
+        for(auto &it:allowedSwaps){
+            int source = it[0]; int destination = it[1];
+            graph[source].push_back(destination);
+            graph[destination].push_back(source); // make a bidirectional graph 
         }
 
-        vector<int> visited(n, 0);
-        vector<int> parent(n, -1);
+        vector<int>visited(source.size(),0);
 
-        for (int i = 0; i < n; i++) {
-            if (!visited[i]) {
-                dfs(i, i, mp, graph, visited, parent, source);
+        for(int i=0;i<source.size();i++){
+            if(!visited[i]){
+                dfs(i,i,mp,graph,visited,parent,source);
             }
         }
+
+        // for(auto &it:mp){
+        //     cout<<"Parent is "<<it.first<<":"<<endl;
+        //     for(auto &j:it.second){
+        //         cout<<j.first<<" "<<j.second<<endl;
+        //     }
+        //     cout<<endl;
+        //     cout<<endl;
+        // }
 
         int ans = 0;
-
-        for (int i = 0; i < n; i++) {
+        for(int i=0;i<target.size();i++){
             int p = parent[i];
 
-            if (mp[p][target[i]] > 0) {
+            if(mp[p][target[i]] > 0){
                 mp[p][target[i]]--;
-            } else {
-                ans++; 
+            }
+            else{
+                ans++;
             }
         }
+
+
 
         return ans;
     }
