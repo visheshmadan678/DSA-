131d00847c6ec5a78834633768558adaf9e839ea
vishesh madan <119437780+visheshmadan678@users.noreply.github.com>
2026-05-25 10:35:09 +0530

    Time: 12 ms (46.82%) | Memory: 33.7 MB (12.82%) - LeetSync

--- 2001-jump-game-vii/jump-game-vii.cpp
+++ 2001-jump-game-vii/jump-game-vii.cpp
@@ -1,23 +1,32 @@
 class Solution {
 public:
-    bool canReach(string s, int minJump, int maxJump) {
-        queue<int> qt;
-        int len = s.length();
-        vector<int> visited(s.length(),0);
-        visited[0] = 1;
-        int seen = 0;
-        qt.push(0); // push the starting index
-        while (!qt.empty()) {
+    bool canReach(string s, int mini, int maxi) {
+        queue<int>qt; int size = s.length(); 
+
+        vector<int>visited(size,0);
+
+        qt.push(0);
+        visited[0] = 1; // mark it as visited
+        int prev_max = INT_MIN;
+
+        while(!qt.empty()){
             int idx = qt.front();
             qt.pop();
-            if (idx == s.length() - 1) return true;
-            for (int i = max(seen,idx+minJump); i <= min(idx + maxJump, len-1);i++) {
-                if (s[i] == '0') {
-                    qt.push(i);
+
+            if(idx == size - 1)return true;
+
+            // if(visited[idx] == 1)continue; // then skip this one
+            for(int k = max(prev_max,idx + mini); k <= min(size - 1, idx + maxi); k++){
+
+                prev_max = max(k,prev_max);
+                if(!visited[k] && s[k] == '0'){
+                    visited[k] = 1;
+                    qt.push(k);
                 }
+                
             }
-            seen = min(idx + maxJump + 1, len);
         }
-        return false;   
+
+        return false;
     }
 };
