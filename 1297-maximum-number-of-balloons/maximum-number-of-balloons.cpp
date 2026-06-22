9b64a489a68e798ab5e8cae27c61114075daa6ae
vishesh madan <119437780+visheshmadan678@users.noreply.github.com>
2026-06-22 11:05:44 +0530

    Time: 3 ms (12.33%) | Memory: 8.9 MB (84.03%) - LeetSync

---
+++ 1297-maximum-number-of-balloons/maximum-number-of-balloons.cpp
@@ -0,0 +1,26 @@
+class Solution {
+public:
+    #define rep(i, n) for (int i = 0; i < (n); i++)
+    int maxNumberOfBalloons(string text) {
+        map<char,long long>mp;
+
+        rep(i,text.length()){
+            mp[text[i]]++; // store the value inside the map
+        }
+
+        long long ans = INT_MAX;
+
+        if(mp['b'] && mp['a'] && mp['l'] >= 2 && mp['o'] >= 2 && mp['n']){
+            ans = min(ans,mp['b']);
+            ans = min(ans,mp['a']);
+            ans = min(ans,mp['l'] / 2);
+            ans = min(ans,mp['o'] / 2);
+            ans = min(ans,mp['n']);
+        }
+        else{
+            return 0;
+        }
+
+        return ans; // return ans in the end 
+    }   
+};
